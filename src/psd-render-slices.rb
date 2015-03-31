161b340629aad4686d1eafccf5aef19b0929f087
kwketh <keth@kwketh.com>
2015-04-01 00:31:46 +0100

    PSD.rb is no longer used to render the png, ImageMagick 'convert' can render psds better with full 8-bit transparency and ignoring the invisible background layer that Photoshop fomat oddly preserves (see http://www.wizards-toolkit.org/discourse-server/viewtopic.php?t=17012)

--- src/psd-render-slices.rb
+++ src/psd-render-slices.rb
@@ -53,8 +53,7 @@ Dir.mkdir outputDir unless File.exists?(outputDir)
 # Render complete psd to temporary file
 renderFile = Tempfile.new('assets.png')
 begin
-    png = psd.image.to_png
-    psd.image.save_as_png(renderFile.path)
+    system("convert -page #{psd.width}x#{psd.height} -background none \"#{psdFile}\" -delete 0 -flatten #{renderFile.path}")
 
     rendered = Hash.new
     duplicates = Hash.new
